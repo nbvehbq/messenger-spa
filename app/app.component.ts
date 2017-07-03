e867120b079873011a049e74d60f3a74f34bafba
Timur Karpenok <nbvehbq@gmail.com>
2017-07-04 00:20:08 +0300

    add delete method

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+const messages = [
+  {
+    sender: 'Robert Lutes',
+    time: '(2016-11-23 21:13:45)',
+    text: 'Hi, Piter',
+    owned: false,
+  },
+  {
+    sender: 'My',
+    time: '(2016-11-23 21:13:49)',
+    text: 'Hi, Robert',
+    owned: true,
+  },
+
+];
+
 @Component({
     moduleId: module.id,
     selector: 'app',
     templateUrl: 'app.component.html',
 })
 export class AppComponent {
+  messages = messages;
 
+  delete(message: any) {
+    let index = this.messages.indexOf(message);
+
+    if ( index > -1 ) {
+      this.messages.splice(index, 1);
+    }
+  }
 }
